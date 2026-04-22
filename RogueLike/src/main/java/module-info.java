df4e60ead438db97b9979508152a6d675347530f
Lorenzo-d-L <lorenzodelaat@ziggo.nl>
2026-04-22 12:05:58 +0200

    Update main application class and refine Maven configuration

--- RogueLike/src/main/java/module-info.java
+++ RogueLike/src/main/java/module-info.java
@@ -1,8 +1,4 @@
 module com.ldldevelopment.roguelike {
     requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.ldldevelopment.roguelike to javafx.fxml;
     exports com.ldldevelopment.roguelike;
 }
